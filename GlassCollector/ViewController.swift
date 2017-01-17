a3d34f455101bab15e3a5a9762840526534c293e
Cristian Curciuc <cristiancurciuc@gmail.com>
2017-01-17 19:42:44 +0100

    no message

---
+++ GlassCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlassCollector
+//
+//  Created by Cristian Curciuc on 17/01/2017.
+//  Copyright © 2017 Cristian Curciuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
